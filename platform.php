eb4e5164f5d178b88f2d5a0d59549c3d8e02ff94
Kyle4Camden <166765824+Kyle4Camden@users.noreply.github.com>
2024-04-11 18:37:39 -0400

    Initial Website Loading
    Taking the site documents off of current server and over.

---
+++ platform.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kyle Irwin - Camden County Board of Commissioners</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav id="navbar">
+        <ul>
+            <li><a href="index.php">ABOUT KYLE</a></li>
+            <li><a href="platform.php">PLATFORM</a></li>
+            <li><a href="contact.php">CONTACT ME</a></li>
+            <li><a href="#">DONATE</a></li>
+        </ul>
+    </nav>
+    <header></header>
+    <div class="container">
+        <div id="paragraph-one">
+            <p>
+                Environment
+            </p>
+            At the top of my local environmental concerns is the Covanta Incinerator in Camden, right by Morgan Village. The incinerator is one of 2 remaining in the country due to a national understanding on environmental harm they cause since the 2000’s. It violates health codes yearly that contaminate the local air with harmful substances such as lead. The people most affected by this are the residents of Camden and the immediate neighboring communities. It is pivotal to close down and replace this facility with something that would benefit the region. A large roadblock in closing it down is the local leadership, all of whom have financial interests in keeping the plant open. I am a student and worker from an average background, I own no investments nor do I intend on being a politician who answers to the highest bidder. I plan to represent the wills of the people I represent. So, as a member of the board of commissioners, I would bring an unbribed opinion to advocate for this facility to finally stop contaminating our community.
+        </div>
+
+        <div id="paragraph-two">
+            <p>
+                Education
+            </p>
+            A key aspect of being on the board of commissioners is the allocation of the budget. As previously mentioned, there are hundreds of millions of dollars being spent without discussion. One argument I would introduce to the budgetary debate would be for more of the budget to be spent on education. This spending would include elevating our library system as well as ensuring less inequality between different school districts. As a student who is fortunate enough to be receiving a higher education, as well as coming from less privileged towns such as Camden and Woodlynne, I have come to the conclusion that improvements to education are tantamount to improving a multitude of aspects in life. Providing less privileged students with access to higher quality education will open the door to more opportunities for them. Less equitable education is a key proponent to why students with lower quality education are more inclined towards crime, which at the moment is punitively punished by an over-funded police force.
+            <br>
+            <br>
+            I am an advocate for reducing the budget of the police force and other defense budgets and injecting more into the education budget. I am arguing that punishing people who were not given a fair chance is not the solution to reducing crime, that is a reaction to it without addressing the symptom: inequity. The problems caused by this inequity, ranging from crime, to homelessness, to people never getting into higher paying jobs, can all be solved first by providing a higher standard of education to residents of the county. From that point, more people can get into the fields they have passion for and we can communally work on these issues as a more equal community. We need to solve the issues that lead to crime and desperation, not punish the people who suffer from it.
+        </div>
+
+        <div id="paragraph-three">
+            <p>
+                Democracy
+            </p>
+            As it currently stands, the Camden County Board of Commissioners decides on how a budget ranging from $540M to $418.5M is spent during the timespan of most people’s lunch breaks, about 30 minutes. These deliberations are open to the public but are held at a time that most people cannot normally attend, on Wednesdays around noon. It is criminal that this large amount of money is being spent without any internal or external discussion to insure that it is being spent in a way to greater benefit the people it is meant to serve. For example, the services provided by the Camden County Board of Social Services was cut in the reduced budget from 2021 into 2022, this decision was made within a half hour without the input of the people who would receive these services. I am running to open these meetings to be more accessible to the public and to personally promote internal debate on these budgetary hearings so that the way the budget is spent can be more positively felt by the residents of Camden County.
+        </div>
+    </div>
+</body>
+</html>
